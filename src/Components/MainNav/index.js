fb9ade5e1fd15558c9d24a57e98eca4b65720809
Paul <paul.bratslavsky@gmail.com>
2019-09-06 19:32:53 -0500

    Basic css complete

--- src/Components/MainNav/index.js
+++ src/Components/MainNav/index.js
@@ -6,15 +6,11 @@ import './mainnav.scss';
 const MainNav = () => {
   return (
     <nav className="main-nav">
-      <div className="container">
-        <div className="mobile-nav__item center">
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/services'>Services</Link>
-          <Link to='/testimonials'>Testimonials</Link>
-          <Link  to='/contact'>Contatct</Link>
-        </div>
-      </div>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/services'>Services</Link>
+      <Link to='/testimonials'>Testimonials</Link>
+      <Link  to='/contact'>Contatct</Link>
     </nav>
   )
 }
